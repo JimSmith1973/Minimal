548c70b902fd2c10a4cc89f1bef20f1bf045f71f
jps1973 <47426983+jps1973@users.noreply.github.com>
2025-01-03 16:14:50 +0000

    Minimal Windows Application
    Shows any input parameters, and then an About message

--- make.bat
+++ make.bat
@@ -2,18 +2,9 @@ cls
 
 del *.exe
 
-windres Resource.rc -o Resource.o
-
-g++ -mwindows -m64 -static -Wall -Wextra Template.cpp ^
+g++ -mwindows -m64 -static -Wall -Wextra Minimal.cpp ^
  ^
  ..\Classes\ArgumentListClass.cpp ^
- ..\Classes\DroppedFilesClass.cpp ^
- ..\Classes\FontClass.cpp ^
- ..\Classes\ListBoxWindowClass.cpp ^
- ..\Classes\MenuClass.cpp ^
  ..\Classes\MessageClass.cpp ^
- ..\Classes\StatusBarWindowClass.cpp ^
- ..\Classes\WindowClass.cpp ^
- ..\Classes\WindowClassClass.cpp ^
  ^
- Resource.o -o Template.exe
+ -o Minimal.exe
